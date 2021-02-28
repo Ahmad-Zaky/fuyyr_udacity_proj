67edae9f770e5ede0848ad97bac29034315ffea6
Ahmed_Zaky <ahmed6mohamed6@gmail.com>
2021-02-28 05:23:07 +0200

    Creating new Model Shows which relate Venues and Artists - created created/modified datetime columns in Show/Venue/Artist models

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.orm import backref
 from forms import *
 from flask_migrate import Migrate
 import sys, traceback
@@ -20,17 +22,30 @@ from re import error
 # App Config.
 #----------------------------------------------------------------------------#
 
+# TODO: connect to a local postgresql database
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-# TODO: connect to a local postgresql database
 migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'shows'
+
+  venue_id = db.Column(db.Integer, ForeignKey('venues.id'), primary_key=True)
+  artist_id = db.Column(db.Integer, ForeignKey('artists.id'), primary_key=True)
+  start_time  = db.Column(db.DateTime, server_default=db.func.now())
+  venue = db.relationship("Venue", back_populates="venues")
+  artist = db.relationship("Artist", back_populates="artists")
+
+  def __repr__(self):
+    return f'<Show Venue ID: {self.venue_id}, Artist ID: {self.artist_id}>'
+
 
 class Venue(db.Model):
     __tablename__ = 'venues'
@@ -46,6 +61,11 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    created_time  = db.Column(db.DateTime, server_default=db.func.now())
+    modified_time = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    def __repr__(self):
+      return f'<Venue ID: {self.id}, Name: {self.name}>'
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -60,8 +80,13 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    created_time  = db.Column(db.DateTime, server_default=db.func.now())
+    modified_time = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    def __repr__(self):
+      return f'<Artist ID: {self.id}, Name: {self.name}>'
+
+
 
 #----------------------------------------------------------------------------#
 # Filters.
