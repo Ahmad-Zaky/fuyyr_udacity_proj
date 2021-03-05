688f3cbe34850dde3effbae8dcddfac14e50d866
Ahmed_Zaky <ahmed6mohamed6@gmail.com>
2021-03-05 11:57:15 +0200

    separted the models in a new file called models.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import backref
 from sqlalchemy.exc import IntegrityError
@@ -31,75 +31,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-class Show(db.Model):
-  __tablename__ = 'shows'
-
-  venue_id = db.Column(db.Integer, ForeignKey('venues.id'), primary_key=True)
-  artist_id = db.Column(db.Integer, ForeignKey('artists.id'), primary_key=True)
-  start_time  = db.Column(db.DateTime, server_default=db.func.now())
-
-  venue = db.relationship("Venue", backref="artist_shows")
-  artist = db.relationship("Artist", backref="venue_shows")
-
-  def __repr__(self):
-    return f'<Show Venue ID: {self.venue_id}, Artist ID: {self.artist_id}>'
-
-
-class Venue(db.Model):
-    __tablename__ = 'venues'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    website = db.Column(db.String(500))
-    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.Text)
-    facebook_link = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-
-    created_time  = db.Column(db.DateTime, server_default=db.func.now())
-    modified_time = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
-
-    artists = db.relationship("Artist", secondary="shows")
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    
-    def __repr__(self):
-      return f'<Venue ID: {self.id}, Name: {self.name}>'
-
-class Artist(db.Model):
-    __tablename__ = 'artists'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    website = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.Text)
-    image_link = db.Column(db.String(500))
-    
-    created_time  = db.Column(db.DateTime, server_default=db.func.now())
-    modified_time = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-    def __repr__(self):
-      return f'<Artist ID: {self.id}, Name: {self.name}>'
-
-
+from models import Venue, Artist, Show
 
 #----------------------------------------------------------------------------#
 # Filters.
---
+++ models.py
@@ -0,0 +1,74 @@
+
+from app import db
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.orm import backref
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'shows'
+
+  venue_id = db.Column(db.Integer, ForeignKey('venues.id'), primary_key=True)
+  artist_id = db.Column(db.Integer, ForeignKey('artists.id'), primary_key=True)
+  start_time  = db.Column(db.DateTime, server_default=db.func.now())
+
+  venue = db.relationship("Venue", backref="artist_shows")
+  artist = db.relationship("Artist", backref="venue_shows")
+
+  def __repr__(self):
+    return f'<Show Venue ID: {self.venue_id}, Artist ID: {self.artist_id}>'
+
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.Text)
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+
+    created_time  = db.Column(db.DateTime, server_default=db.func.now())
+    modified_time = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    artists = db.relationship("Artist", secondary="shows")
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    
+    def __repr__(self):
+      return f'<Venue ID: {self.id}, Name: {self.name}>'
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.Text)
+    image_link = db.Column(db.String(500))
+    
+    created_time  = db.Column(db.DateTime, server_default=db.func.now())
+    modified_time = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+    def __repr__(self):
+      return f'<Artist ID: {self.id}, Name: {self.name}>'
+
+
